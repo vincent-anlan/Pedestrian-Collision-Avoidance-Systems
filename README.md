db0d365fc5b085b668f6662eeea24990d3a80d93
Yueqing Sheng <shengyueqing@gmail.com>
2019-10-10 16:57:46 -0400

    init flutter model

---
+++ README.md
@@ -0,0 +1,29 @@
+# Pedestrian-Collision-Avoidance-Systems
+## Team: Road Hackers
+
+*bold*
+**italic**
+* 1
+* 2
+
+## Install 
+
+```
+flutter packages get
+```
+
+## Run
+
+```
+flutter run
+```
+
+## Models
+
+- Image Classification
+  - MobileNet
+
+- Object Detection
+  - SSD MobileNet
+  - Yolov2 Tiny
+
---
+++ lib/home.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:tflite/tflite.dart';
+import 'dart:math' as math;
+
+import 'camera.dart';
+import 'bndbox.dart';
+import 'models.dart';
+
+class HomePage2 extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  HomePage2(this.cameras);
+
+  @override
+  _HomePage2State createState() => new _HomePage2State();
+}
+
+class _HomePage2State extends State<HomePage2> {
+  List<dynamic> _recognitions;
+  int _imageHeight = 0;
+  int _imageWidth = 0;
+  String _model = "";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  setRecognitions(recognitions, imageHeight, imageWidth) {
+    setState(() {
+      _recognitions = recognitions;
+      _imageHeight = imageHeight;
+      _imageWidth = imageWidth;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size screen = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+              children: [
+                Camera(
+                  widget.cameras,
+                  _model,
+                  setRecognitions,
+                ),
+                BndBox(
+                    _recognitions == null ? [] : _recognitions,
+                    math.max(_imageHeight, _imageWidth),
+                    math.min(_imageHeight, _imageWidth),
+                    screen.height,
+                    screen.width,
+                    _model),
+              ],
+            ),
+    );
+  }
+}
