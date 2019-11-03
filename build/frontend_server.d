2ce6712ba26dc09b4a010fb36fc0f769f82e0927
anlan <anlan9933@gmail.com>
2019-11-02 22:41:24 -0400

    new config

--- build/frontend_server.d
+++ build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: /Applications/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
+frontend_server.d: C:\src\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
