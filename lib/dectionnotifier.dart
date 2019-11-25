62bd609d746cdcf5044ece4d5543505ea8313a27
Lin Yang <linayanglin@gmail.com>
2019-11-24 19:48:05 -0500

    Update warning sound effect to local. - always refresh audio before play new sound.

--- lib/dectionnotifier.dart
+++ lib/dectionnotifier.dart
@@ -1,8 +1,11 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:road_hackers/sharabledata.dart';
 
 class DetectionNotifier extends ValueNotifier<SharableData> {
+  static AudioCache player = AudioCache();
+  
   DetectionNotifier(SharableData value) : super(value);
 
   void changeDectionMsg(String msg) {
@@ -13,9 +16,9 @@ class DetectionNotifier extends ValueNotifier<SharableData> {
   void changeDectionColor(Color color) {
     value.color = color;
     notifyListeners();
-    // if (color == Colors.red) {
-    //   AudioPlayer audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
-    //   audioPlayer.play("http://downsc.chinaz.net/Files/DownLoad/sound1/201411/5185.mp3");
-    // }
+    if (color == Colors.red) {
+      player.clearCache();
+      player.play('warningSound.mp3');
+    }
   }
 }
